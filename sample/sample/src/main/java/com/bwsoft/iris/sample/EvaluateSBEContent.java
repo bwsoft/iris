28aa0271f1192d402b640b1cfba85a31eb505240
Yuan Zhou <yuan60101@gmail.com>
2016-08-02 15:33:21 -0500

    rename artifact to be the message

---
+++ sample/sample/src/main/java/com/bwsoft/iris/sample/EvaluateSBEContent.java
@@ -0,0 +1,9 @@
+package com.bwsoft.iris.sample;
+
+import com.bwsoft.iris.message.SBEMessageSchema;
+
+public class EvaluateSBEContent {
+	public EvaluateSBEContent(String sbeXMLSchema) {
+//		SBEMessageSchema
+	}
+}

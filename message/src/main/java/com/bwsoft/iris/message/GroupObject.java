9219e30686e5d32ca3221eadf7b604ce0a5d440d
Yuan Zhou <yuan60101@gmail.com>
2016-08-10 14:53:35 -0500

    fix copyright

--- message/src/main/java/com/bwsoft/iris/message/GroupObject.java
+++ message/src/main/java/com/bwsoft/iris/message/GroupObject.java
@@ -5,13 +5,13 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  *******************************************************************************/
 package com.bwsoft.iris.message;
 
@@ -344,6 +344,15 @@ public interface GroupObject {
 	 */
 	public int setBytes(Field field, byte[] src, int srcOffset, int length);
 	
+	/**
+	 * Adjust the size of a raw field without modifying its content. If the underlying field
+	 * is not a raw field, an UnsupportedOperationException is thrown.
+	 * 
+	 * @param field a raw field
+	 * @param length its new length
+	 */
+	public void adjustSizeForRawField(Field field, int length);
+
 	/**
 	 * Return a number array of this field. The inference generic type needs to be able to hold the
 	 * returned value. Otherwise a cast exception will be thrown.
--- message/src/main/java/com/bwsoft/iris/message/sbe/SBEObject.java
+++ message/src/main/java/com/bwsoft/iris/message/sbe/SBEObject.java
@@ -5,13 +5,13 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  *******************************************************************************/
 package com.bwsoft.iris.message.sbe;
 
@@ -489,6 +489,20 @@ class SBEObject implements GroupObject {
 		} else {
 			throw new IllegalArgumentException("field, "+field.getID()+", does not belong to this group, "+this.getDefinition().getID());
 		}		
+	}
+	
+	@Override
+	public void adjustSizeForRawField(Field field, int length) {
+		if( FieldType.RAW == field.getType() ) {
+			SBEObjectArray objArray = this.childFields.get(field.getID());
+			if( null != objArray ) {
+				objArray.adjustRawGroupSize(length);
+			} else {
+				throw new InternalError("internal error: unrecognized raw field");
+			}		
+		} else {
+			throw new UnsupportedOperationException("not applicable to fields other than RAW");
+		}
 	}
 
 	@Override

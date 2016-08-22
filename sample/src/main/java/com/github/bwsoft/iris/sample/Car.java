80a4327af7a744cb952f5c5f3810ae757c3c8703
Yuan Zhou <yuan60101@gmail.com>
2016-08-22 14:39:27 -0500

    change to getField(String fieldName) instead of id in the sample code for better understanding

--- sample/src/main/java/com/github/bwsoft/iris/sample/Car.java
+++ sample/src/main/java/com/github/bwsoft/iris/sample/Car.java
@@ -97,75 +97,75 @@ public class Car {
 		System.out.println("Empty message: "+MessageUtil.toJsonString(msgObj));
 		
 		// Field: serialNumber - set up a number field of uint64
-		msgObj.setNumber(msgObj.getField((short) 1), 1234567); 
+		msgObj.setNumber(msgObj.getField("serialNumber"), 1234567); 
 
 		// Field: modelYear - set up a number field of unit16
-		msgObj.setNumber(msgObj.getField((short) 2), 2016); 
+		msgObj.setNumber(msgObj.getField("modelYear"), 2016); 
 
 		// Field: available - set up an enum of type uint8
-		msgObj.setNumber(msgObj.getField((short) 3), BooleanType.TRUE.value()); 
+		msgObj.setNumber(msgObj.getField("available"), BooleanType.TRUE.value()); 
 
 		// Field: code - set up an enum of type char.
-		msgObj.setChar(msgObj.getField((short) 4),Model.C.value()); 
+		msgObj.setChar(msgObj.getField("code"),Model.C.value()); 
 
 		// Field: somenumber - set up a number array of type int[5]
 		Integer[] someNumbers = {1,2,10,4,5};
-		msgObj.setNumbers(msgObj.getField((short) 5), someNumbers, 0, 5); 
+		msgObj.setNumbers(msgObj.getField("someNumbers"), someNumbers, 0, 5); 
 
 		// Field: vehicleCode - set up a char array of char[6].
 		char[] vehicleCode = {'m','y','c','o','d','e'}; 
-		msgObj.setChars(msgObj.getField((short) 6), vehicleCode, 0, 6); 
+		msgObj.setChars(msgObj.getField("vehicleCode"), vehicleCode, 0, 6); 
 		
 		// Field: extra - set up a bitset of uint8
 		BitSet bitSet = new BitSet();
 		bitSet.set(0);
 		bitSet.set(2);
 		byte value = bitSet.toByteArray()[0];
-		msgObj.setByte(msgObj.getField((short) 7), value);
+		msgObj.setByte(msgObj.getField("extras"), value);
 		
 		// Field: engine - set up a composite field. Ignore all constant fields.
 		Group engine = (Group) msgObj.getField((short) 8);
-		msgObj.setNumber(engine.getField((short) 0), 1500); // Field: capacity - the first field in the composite field 
-		msgObj.setNumber(engine.getField((short) 1), 6); // Field: numCylinders - the second field in the composite
+		msgObj.setNumber(engine.getField("capacity"), 1500); // Field: capacity - the first field in the composite field 
+		msgObj.setNumber(engine.getField("numCylinders"), 6); // Field: numCylinders - the second field in the composite
 		// Jump to the 3rd field to skip the constant field. 
 		char[] manufactureCode = {'V','T','I'};
-		msgObj.setChars(engine.getField((short) 3), manufactureCode, 0, 3); // Field: manufactureCode - the third field in the composite.
+		msgObj.setChars(engine.getField("manufacturerCode"), manufactureCode, 0, 3); // Field: manufactureCode - the third field in the composite.
 
 		// Field: fuelFigures - set up a group
-		GroupObjectArray fuelFigures = msgObj.getGroupArray(msgObj.getField((short) 9));
+		GroupObjectArray fuelFigures = msgObj.getGroupArray(msgObj.getField("fuelFigures"));
 		GroupObject fuelFigure = fuelFigures.addGroupObject(); // add a row to the group
-		fuelFigure.setNumber(fuelFigure.getField((short) 10), 40); // Field: speed 
-		fuelFigure.setNumber(fuelFigure.getField((short) 11), 25.5); // Field: mpg
+		fuelFigure.setNumber(fuelFigure.getField("speed"), 40); // Field: speed 
+		fuelFigure.setNumber(fuelFigure.getField("mpg"), 25.5); // Field: mpg
 		
 		// Field: performanceFigures - set up a group
-		GroupObjectArray performanceFigures = msgObj.getGroupArray(msgObj.getField((short) 12));
+		GroupObjectArray performanceFigures = msgObj.getGroupArray(msgObj.getField("performanceFigures"));
 		GroupObject performanceFigure = performanceFigures.addGroupObject(); // add a row to the group
-		performanceFigure.setNumber(performanceFigure.getField((short) 13), 89); // Field: octaneRating
+		performanceFigure.setNumber(performanceFigure.getField("octaneRating"), 89); // Field: octaneRating
 		
 		// Field: acclerations - set up a group within another group
-		GroupObjectArray acclerations = performanceFigure.getGroupArray(performanceFigure.getField((short) 14));
+		GroupObjectArray acclerations = performanceFigure.getGroupArray(performanceFigure.getField("acceleration"));
 		GroupObject accleration = acclerations.addGroupObject();
-		accleration.setNumber(accleration.getField((short) 15), 60);
-		accleration.setNumber(accleration.getField((short) 16), 2.5);
+		accleration.setNumber(accleration.getField("mph"), 60);
+		accleration.setNumber(accleration.getField("seconds"), 2.5);
 		
 		// Field: fuelFigures - to add another row in the previous repeating group.
 		// It is a good practice to add fields in sequence. But not necessary.
 		fuelFigure = fuelFigures.addGroupObject(); // add a row to the group
-		fuelFigure.setNumber(fuelFigure.getField((short) 10), 60); // Field: speed 
-		fuelFigure.setNumber(fuelFigure.getField((short) 11), 30); // Field: mpg
+		fuelFigure.setNumber(fuelFigure.getField("speed"), 60); // Field: speed 
+		fuelFigure.setNumber(fuelFigure.getField("mpg"), 30); // Field: mpg
 		
 		// Field: model - set up a raw field. It is good to follow the order of a raw field. 
 		// But not necessary.
 		byte[] model = "Accord".getBytes();
-		msgObj.setBytes(msgObj.getField((short) 18), model, 0, model.length);
+		msgObj.setBytes(msgObj.getField("model"), model, 0, model.length);
 
 		// Field: make - set up a raw field
 		byte[] make = "Honda".getBytes();
-		msgObj.setBytes(msgObj.getField((short) 17), make, 0, make.length);
+		msgObj.setBytes(msgObj.getField("make"), make, 0, make.length);
 		
 		// Field: activationCode - set up a raw field
 		byte[] activationCode = "deadbeef".getBytes();
-		msgObj.setBytes(msgObj.getField((short) 19), activationCode, 0, activationCode.length);
+		msgObj.setBytes(msgObj.getField("activationCode"), activationCode, 0, activationCode.length);
 		
 		// print out the message
 		System.out.println("Created message:"+MessageUtil.toJsonString(msgObj));
@@ -191,14 +191,14 @@ public class Car {
 
 		// Field: available - get an enum of type uint8
 		// Option 1: to get its value
-		Number availableValue = msgObj.getNumber(msgObj.getField((short) 3)); 
+		Number availableValue = msgObj.getNumber(msgObj.getField("available")); 
 		// Option 2: to get its name, but slower in performance due to a Hashmap lookup.
-		String availableEnumName = msgObj.getEnumName(msgObj.getField((short) 3)); 
+		String availableEnumName = msgObj.getEnumName(msgObj.getField("available")); 
 		System.out.println("Field: available, Enum Value: "+availableValue.shortValue()+", Enum Name: "+availableEnumName);
 	
 		// Field: someNumbers - get a number array
 		Integer[] someNumbers = new Integer[5];
-		msgObj.getNumbers(msgObj.getField((short) 5), someNumbers, 0, 5);
+		msgObj.getNumbers(msgObj.getField("someNumbers"), someNumbers, 0, 5);
 		System.out.print("Field: someNumbers, Value: ");
 		for( int i = 0; i < someNumbers.length; i ++ ) {
 			System.out.format("%d ", someNumbers[i]);
@@ -207,15 +207,15 @@ public class Car {
 		
 		// Field: extra - get a bit choice field
 		// Option 1: to get its value
-		Number extraValue = msgObj.getNumber(msgObj.getField((short) 7));
+		Number extraValue = msgObj.getNumber(msgObj.getField("extras"));
 		// Option 2: to get the bit set of all options (slower)
-		boolean sunRoof = msgObj.isSet(msgObj.getField((short) 7), "sunRoof");
-		boolean sportsPack = msgObj.isSet(msgObj.getField((short) 7), "sportsPack");
-		boolean cruiseControl = msgObj.isSet(msgObj.getField((short) 7), "cruiseControl");
+		boolean sunRoof = msgObj.isSet(msgObj.getField("extras"), "sunRoof");
+		boolean sportsPack = msgObj.isSet(msgObj.getField("extras"), "sportsPack");
+		boolean cruiseControl = msgObj.isSet(msgObj.getField("extras"), "cruiseControl");
 		System.out.println("Field: extra, Value: "+extraValue.intValue()+", sunRoof? "+sunRoof+", sportsPack? "+sportsPack+" ,cruiseControl? "+cruiseControl);
 
 		// Field: engine - get a composite field
-		Group engine = (Group) msgObj.getField((short) 8);
+		Group engine = (Group) msgObj.getField("engine");
 		Number capacity = msgObj.getNumber(engine.getField((short) 0)); // Field: capacity - the first field in the composite field 
 		Number numCylinders = msgObj.getNumber(engine.getField((short) 1)); // Field: numCylinders - the second field in the composite
 		String maxRpm = msgObj.getString(engine.getField((short) 2)); // Field: maxRpm, a constant field. 
@@ -230,20 +230,20 @@ public class Car {
 		
 		// Field: model - get a raw field
 		byte[] model = new byte[10];
-		msgObj.getBytes(msgObj.getField((short) 18), model, 0, model.length);
+		msgObj.getBytes(msgObj.getField("model"), model, 0, model.length);
 		System.out.println("Field: model, Value: "+new String(model));
 		
 		// Field: performanceFigures - get a repeating group
-		GroupObjectArray performanceFigures = msgObj.getGroupArray(msgObj.getField((short) 12));
+		GroupObjectArray performanceFigures = msgObj.getGroupArray(msgObj.getField("performanceFigures"));
 		GroupObject performanceFigure = performanceFigures.getGroupObject(0); // get the first row
-		Number octaneRating = performanceFigure.getNumber(performanceFigure.getField((short) 13));
+		Number octaneRating = performanceFigure.getNumber(performanceFigure.getField("octaneRating"));
 		System.out.println("Field: performanceFigures[0].octaneRating, Value: "+octaneRating.intValue());
 
 		// Field: acclerations - a nested repeating group
-		GroupObjectArray acclerations = performanceFigure.getGroupArray(performanceFigure.getField((short) 14));
+		GroupObjectArray acclerations = performanceFigure.getGroupArray(performanceFigure.getField("acceleration"));
 		GroupObject accleration = acclerations.getGroupObject(0);
-		Number mph = accleration.getNumber(accleration.getField((short) 15));
-		Number seconds = accleration.getNumber(accleration.getField((short) 16));
+		Number mph = accleration.getNumber(accleration.getField("mph"));
+		Number seconds = accleration.getNumber(accleration.getField("seconds"));
 		System.out.println("Field: performanceFigures[0].accleration[0], Value: mph="+mph.intValue()+", seconds="+seconds.floatValue());
 	}
 	

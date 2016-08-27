9e4b6fc3dccbcb2d17124bcf5019eee5b9c657d3
Yuan Zhou <yuan60101@gmail.com>
2016-08-26 19:23:49 -0500

    empty out the old arch to have a clean space for the new work

--- iris/src/main/java/com/bunny/iris/message/sbe/SBEHeader.java
+++
@@ -1,5 +0,0 @@
-package com.bunny.iris.message.sbe;
-
-public interface SBEHeader {
-	public short getHeaderSize();
-}
--- iris/src/main/java/com/bunny/iris/util/Resource.java
+++
@@ -1,5 +0,0 @@
-package com.bunny.iris.util;
-
-public interface Resource {
-	
-}
